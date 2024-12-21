3a3c69141b1ca6f2e17aa9b2cd796118e57e4cc1
Thirumalaibarath <162014344+Thirumalaibarath@users.noreply.github.com>
2024-12-21 17:52:14 +0530

    Update_4 server.js

--- server.js
+++ server.js
@@ -87,9 +87,7 @@ app.get('/redirect', (req, res) => {
             if (data.id_token) {
                 req.session.idToken = data.id_token;
                 console.log(data.id_token)
-                // res.status(200).json({ id_token: req.session.idToken });
-                const redirectUrl = `https://alma-matar.onrender.com/success?id_token=${idToken}`;
-                res.redirect(redirectUrl);
+                res.status(200).json({ id_token: req.session.idToken });
             } else {
                 res.status(503).json({ error: 'ID token not available.' });
             }
