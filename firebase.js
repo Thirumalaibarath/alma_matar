174d3b49311ddee6067b4a7a9903e8aef1f801af
Thirumalaibarath <162014344+Thirumalaibarath@users.noreply.github.com>
2024-12-29 01:51:13 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,6 +1,7 @@
 const { admin,initializeApp, cert } = require('firebase-admin/app');
 const { getFirestore } = require('firebase-admin/firestore');
-const serviceAccount = JSON.parse(process.env.FIREBASE_PRIVATE_KEY);
+const decodedKey = Buffer.from(process.env.FIREBASE_PRIVATE_KEY, 'base64').toString('utf8');
+const serviceAccount = JSON.parse(decodedKey);
 
 // Initialize Firebase
 initializeApp({
