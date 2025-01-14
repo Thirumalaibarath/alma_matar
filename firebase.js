b4b8503638978c316a1d155bae5a25c30c3201b3
Thirumalaibarath <thirumalaibarath.nitt@gmail.com>
2025-01-14 21:45:47 +0530

    message: updated redirect page

--- firebase.js
+++ firebase.js
@@ -1,10 +1,10 @@
 const { admin,initializeApp, cert } = require('firebase-admin/app');
 const { getFirestore } = require('firebase-admin/firestore');
-// const decodedKey = Buffer.from(process.env.ALMA_MATER_KEY, 'base64').toString('utf8');
+const decodedKey = Buffer.from(process.env.ALMA_MATER_KEY, 'base64').toString('utf8');
 // to be used in serviceAccount variable 
-//   require('./journalia-private-key.json')
+//   require('./journalia-private-key.json')require('./journalia-private-key.json')
 // JSON.parse(decodedKey);
-const serviceAccount =  require('./journalia-private-key.json')
+const serviceAccount =  JSON.parse(decodedKey);
 initializeApp({
   credential: cert(serviceAccount),
 });
